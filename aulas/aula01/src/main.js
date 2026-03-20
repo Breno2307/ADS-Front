f547cc6a45ef82759da35018662e5a51d357a553
Breno Ferreira Candido <breno.ferreira.candido@gmail.com>
2026-03-19 21:49:02 -0300

    implementei validação do login

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,13 @@
-import './style.css'
+import "./style.css";
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", (e) => {
+  const matriculaInput = document.querySelector("#matricula");
+  const senhaInput = document.querySelector("#senha");
+
+  if (matriculaInput.value == "") {
+    console.log("matricula é obrigatoria");
+    return;
+  }
+});
