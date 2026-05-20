ab0e3c06bf0b97f2415eed4cc9d07dc46deb49c4
Breno Ferreira Candido <breno.ferreira.candido@gmail.com>
2026-05-19 21:57:31 -0300

    aula04 implementando perfil obs:validacao da data nao funcionando

--- aulas/aula04/src/Pages/Perfil.jsx
+++ aulas/aula04/src/Pages/Perfil.jsx
@@ -1,9 +1,77 @@
 import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
 
 function Perfil() {
   const { id } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  return <h1>Perfil do usuario {id}</h1>;
+  const salvar = (data) => console.log(data);
+
+  const regras = {
+    nome: {
+      required: "nome é obrigatorio",
+      minLength: { value: 3, message: "Nome deve ter mais de 3 caracteres" },
+      maxLength: {
+        value: 100,
+        message: "Nome deve ter no maximo 100 caracteres",
+      },
+    },
+    email: {
+      required: "email é obrigatorio",
+      patter: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: "email invalido",
+      },
+    },
+    nascimento: {
+      validate: {
+        dataMinima: (value) =>
+          Date.parse(value) > new Date(1899, 11, 31).getTime() || "Data inferior",
+        dataMaxima: (value) =>
+          Date.parse(value) < new Date().getTime() || "Data superior",
+      },
+    },
+    telefone: {
+      sin: { value: 0, message: "telefone invalido" },
+      patter: {
+        value: /^(\(?\d{2}\)?[\s.-]?)?(\d{4,5})[\s.-]?(\d{4})$/,
+        message: "telefone invalido",
+      },
+    },
+  };
+
+  return (
+    <>
+      <h1>Perfil do usuario {id}</h1>
+      <form onSubmit={handleSubmit(salvar)}>
+        <div>
+          <label htmlFor="nome">nome</label>
+          <input type="text" id="nome" {...register("nome", regras.nome)} />
+          {errors?.nome && <p>{errors.nome.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="email">email</label>
+          <input type="email" id="email" {...register("email", regras.email)} />
+          {errors?.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="nascimento">data de nascimento</label>
+          <input type="date" id="nascimento" {...register("nascimento")} />
+          {errors?.nascimento && <p>{errors.nascimento.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="telefone">telefone</label>
+          <input type="tel" id="telefone" {...register("telefone")} />
+          {errors?.telefone && <p>{errors.telefone.message}</p>}
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
+    </>
+  );
 }
 
 export default Perfil;
