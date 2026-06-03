31906b800f4b2b2befdc95a6fdff3f19d3aa27f1
Breno Ferreira Candido <breno.ferreira.candido@gmail.com>
2026-06-02 21:53:21 -0300

    implementando servico de api

---
+++ aulas/aula06/src/pages/Listagem.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { listar } from "../services/ProdutoService";
+
+function Listagem() {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    const disparar = async () => {
+      const resposta = await listar();
+      setDados(resposta);
+    };
+    disparar();
+  }, []);
+  return (
+    <>
+      <h1>Listagem de produtos</h1>
+      <Link to="/produtos/novo">+ adicionar</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Unidade</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nome}</td>
+              <td>{item.preco}</td>
+              <td>{item.unidade}</td>
+              <td>
+                <Link to={`/produto/editar/${item.id}`}></Link>Editar{" "}
+              </td>
+              <Link to="/produtos"></Link>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Listagem;
